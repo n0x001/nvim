822a45cc5fc298874f0170216c8e92878e47b6e5
N1 <d1286432@gmail.com>
2025-04-23 00:34:41 +0200

    Yank into the system clipboard with Y

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,9 +3,9 @@ vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 
 local opts = { noremap = true, silent = true } 
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
-vim.keymap.set("v", "Y", "\"*y", opts)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts) -- Move the selection 1 line up
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts) -- Move the selection 1 line down
+vim.keymap.set("v", "Y", "\"+y", opts)             -- Copy the selection into the system clipboard
 
 vim.keymap.set("n", "<leader>ra", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
 
