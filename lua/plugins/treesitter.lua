d9aef3cf38597f6372d0366f117d2408a55acd58
N1 <d1286432@gmail.com>
2025-04-23 00:36:10 +0200

    Add languages I use the most to the treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,39 @@
 return {
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {  -- Install Python and Lua parsers
+          "python",
+          "lua",
+          "git_config",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "html",
+          "javascript",
+          "json",
+          "luadoc",
+          "luap",
+          "luau",
+          "make",
+          "nginx",
+          "regex",
+          "sql",
+          "ssh_config",
+          "toml",
+          "typescript",
+          "yaml",
+        },         sync_install = false,
+        highlight = {
+          enable = true, -- Enable syntax highlighting
+          additional_vim_regex_highlighting = false, -- Disable regex highlighting
+        },
+        indent = {
+          enable = true, -- Enable Tree-sitter-based indentation
+        },
+      })
+    end
+  },
 }
