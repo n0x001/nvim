ba210a1205f7b123583af3096e714c673c8f455b
N1 <d1286432@gmail.com>
2025-04-24 12:46:54 +0200

    Set vim.opt.scrolloff = 8

--- lua/config/opt.lua
+++ lua/config/opt.lua
@@ -1,8 +1,7 @@
 function set_tabstop(tabstop)
   local ts = tabstop or 2
   vim.opt.tabstop = ts
-
-  -- Optional: Set related options
+-- Optional: Set related options
   vim.opt.shiftwidth = ts
   vim.opt.expandtab = true
   vim.opt.softtabstop = ts
@@ -12,10 +11,13 @@ end
 set_tabstop(2)
 
 -- Other useful settings
-vim.opt.number = true            -- Show line numbers
-vim.opt.relativenumber = true    -- Show up and down line numbers relatively to the current
-vim.opt.autoindent = true        -- Enable auto-indentation
+-- for more details launch :options
+vim.opt.number = true           -- Show line numbers
+vim.opt.relativenumber = true   -- Show up and down line numbers relatively to the current
+vim.opt.autoindent = true       -- Enable auto-indentation
 vim.opt.wrap = true
+
+vim.opt.scrolloff = 8
 
 -- vim.cmd.colorscheme "gruvbox"
 
